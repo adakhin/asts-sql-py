885ebb8d2ec265fa13646e3484b81dca8ec26085
Anthony Dakhin <anthony.dakhin@gmail.com>
2022-02-06 18:45:49 +0300

    test program added

---
+++ test/main.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
+
+#include "../src/asts_sql.h"
+
+namespace pt=boost::property_tree;
+
+int main() {
+  std::string connect_str, errormsg;
+  pt::ptree tree;
+  pt::read_ini("config.ini", tree);
+  for(auto v : tree.get_child("TSMR"))
+    connect_str.append(v.first+"="+v.second.data()+"\r\n");
+  ad::asts::AstsConnection asts;
+  if(!asts.Connect("TE", connect_str, errormsg)) {
+    std::cout<< errormsg << std::endl;
+    return 1;
+  }
+  return 0;
+}
