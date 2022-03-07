418256417c432b51b7424dd0b26fa88c9c57e8c5
Anthony Dakhin <anthony.dakhin@gmail.com>
2022-03-07 18:39:56 +0300

    Python binding: implement high-level Query logic

--- src/python_proxy.cc
+++ src/python_proxy.cc
@@ -3,18 +3,60 @@
 #include "asts_connection.h"
 #include "storage/sqlite.h"
 
-using namespace boost::python;
-using asts_connection_t = ad::asts::AstsConnection<ad::asts::SQLiteStorage>;
+namespace bpy = boost::python;
 
-BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(asts_connection_t_overloads, OpenTable, 1, 2)
+class AstsConnectionProxy: public ad::asts::AstsConnection<ad::asts::SQLiteStorage> {
+public:
+  bpy::list Query(const std::string& query) {
+    bpy::list tmp;
+    ad::asts::SqlResult result;
+    AstsConnection::Query(query, result);
+    size_t fldcount = result.fields.size();
+    for(auto & row : result.data) {
+      bpy::dict line;
+      for(size_t i=0; i<fldcount; ++i) {
+        line[result.fields[i].name] ;
+        if(!row[i].has_value())
+          line[result.fields[i].name] = bpy::object();
+        else
+          switch(result.fields[i].type) {
+            case ad::asts::AstsFieldType::kInteger:
+              line[result.fields[i].name] = bpy::long_(std::any_cast<int64_t>(row[i]));
+              break;
+            case ad::asts::AstsFieldType::kChar:
+            case ad::asts::AstsFieldType::kFloat:
+            case ad::asts::AstsFieldType::kDate:
+            case ad::asts::AstsFieldType::kTime:
+              line[result.fields[i].name] = bpy::str(std::any_cast<std::string>(row[i]));
+              break;
+            case ad::asts::AstsFieldType::kFixed:
+            case ad::asts::AstsFieldType::kFloatPoint:
+              line[result.fields[i].name] = bpy::long_(std::any_cast<double>(row[i]));
+              break;
+            default:
+              break;
+          }
+      }
+      tmp.append(line);
+    }
+    return tmp;
+  }
+
+  void OpenTable(const std::string tablename, std::map<std::string, std::string> inparams={}) {
+     // making BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS happy
+     AstsConnection::OpenTable(tablename, inparams);
+  }
+};
+
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(AstsConnectionProxy_overloads, OpenTable, 1, 2)
 
 BOOST_PYTHON_MODULE(astslib)
 {
-    class_<asts_connection_t>("AstsConnectionProxy")
-        .def("Connect", &asts_connection_t::Connect)
-        .def("Disconnect", &asts_connection_t::Disconnect)
-        .def("OpenTable", &asts_connection_t::OpenTable, asts_connection_t_overloads())
-        .def("CloseTable", &asts_connection_t::CloseTable)
-        .def("Query", &asts_connection_t::Query)
+    bpy::class_<AstsConnectionProxy>("AstsConnectionProxy")
+        .def("Connect", &AstsConnectionProxy::Connect)
+        .def("Disconnect", &AstsConnectionProxy::Disconnect)
+        .def("OpenTable", &AstsConnectionProxy::OpenTable, AstsConnectionProxy_overloads())
+        .def("CloseTable", &AstsConnectionProxy::CloseTable)
+        .def("Query", &AstsConnectionProxy::Query)
     ;
 }
